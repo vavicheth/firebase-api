fc5a51505e45ac1a30f4b2d4b83078a37b78aecb
unknown <vicheth_it@yahoo.com>
2019-09-14 14:56:43 +0700

    first firebase api

---
+++ lib/screens/news_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NewsScreen extends StatefulWidget {
+  @override
+  _NewsScreenState createState() => _NewsScreenState();
+}
+
+class _NewsScreenState extends State<NewsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(),
+
+    );
+  }
+
+  _buildBody() {
+    return Container(
+      color: Colors.lightBlueAccent,
+    );
+  }
+}
