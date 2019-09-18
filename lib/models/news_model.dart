995160b60b2635b1bf0038ce276d3c1bbe48c8ca
unknown <vicheth_it@yahoo.com>
2019-09-18 18:27:53 +0700

    firebase CRUD

--- lib/models/news_model.dart
+++ lib/models/news_model.dart
@@ -1,1 +1,71 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class News{
+  String title;
+  String content;
+  String imageUrl;
+  String postDate;
+  DocumentReference reference;
+  Photo photo;
+  List<Comment> comments;
+
+  News({this.title, this.content, this.imageUrl, this.postDate, this.photo, this.comments, this.reference});
+
+  News.fromMap(Map map, {this.reference}){
+    title = map["title"] ?? "nodata";
+    content = map["content"] ?? "nodata";
+    imageUrl = map["imageUrl"] ?? "nodata";
+    postDate = map["postDate"] ?? "nodata";
+    photo = Photo.fromMap(map["photos"]);
+    comments = List<Comment>.from(map["comments"].map((x) => Comment.fromMap(x)));
+  }
+
+  factory News.fromSnapshot(DocumentSnapshot snapshot){
+    return News.fromMap(snapshot.data, reference: snapshot.reference);
+  }
+
+  Map<String, dynamic> toMap() =>{
+    "title": title ?? "nodata",
+    "content": content ?? "nodata",
+    "imageUrl": imageUrl ?? "nodata",
+    "postDate": postDate ?? "nodata",
+    "photos": photo.toMap(),
+    "comments" : List<dynamic>.from(comments.map((x) => x.toMap())),
+  };
+}
+
+class Photo{
+  String large, small;
+
+  Photo({this.large, this.small});
+
+  factory Photo.fromMap(Map map){
+    return Photo(
+      large: map['large'] ?? "nolarge",
+      small: map['small'] ?? "nosmall",
+    );
+  }
+
+  Map<String, dynamic> toMap() =>{
+    "large": large ?? "nodata",
+    "small": small ?? "nodata",
+  };
+}
+
+class Comment{
+  String commenter;
+  String text;
+  Comment({this.commenter = "nodata", this.text = "nodata"});
+
+  factory Comment.fromMap(Map map){
+    return Comment(
+      commenter: map['commenter'] ?? "nodata",
+      text: map['text'] ?? "nodata",
+    );
+  }
+
+  Map<String, dynamic> toMap() => {
+    "commenter" : commenter ?? "nodata",
+    "text" : text ?? "nodata",
+  };
+}
--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_api/models/news_model.dart';
 import 'package:flutter/material.dart';
 
 class NewsScreen extends StatefulWidget {
@@ -9,14 +11,117 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: _buildAppBar(),
       body: _buildBody(),
-
     );
   }
 
   _buildBody() {
     return Container(
-      color: Colors.lightBlueAccent,
+      color: Colors.grey[200],
+      child: _buildFirestore(),
     );
   }
+
+  _buildFirestore() {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(bottom: 5.0),
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('news').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting: return Center(child: CircularProgressIndicator());
+            default:
+              return _buildListView(snapshot.data.documents);
+          }
+        },
+      ),
+    );
+  }
+
+  _buildListView(List<DocumentSnapshot> documents) {
+    return Container(
+      color: Colors.grey[200],
+      child: ListView.builder(
+          itemCount: documents.length,
+          itemBuilder: (_, index) {
+            News news = News.fromSnapshot(documents[index]);
+            return _buildListViewItem(news);
+          }),
+    );
+  }
+
+  _buildListViewItem(News newsAtIndex){
+    return InkWell(
+      onDoubleTap: (){
+        print("double tap");
+        Firestore.instance.runTransaction((tran) async{
+          var snapshot = await tran.get(newsAtIndex.reference);
+          News news = News.fromSnapshot(snapshot);
+          news.photo.large += ",a,";
+          tran.update(news.reference, news.toMap());
+        });
+      },
+      child: Container(
+        color: Colors.white,
+        margin: EdgeInsets.only(bottom: 5.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+                width: 300.0,
+                height: 50.0,
+                child: Text("commenter: ${newsAtIndex.comments[0].commenter} and text: ${newsAtIndex.comments[0].text}")
+            ),
+            Container(
+                width: 300.0,
+                height: 50.0,
+                child: Text("photo large: ${newsAtIndex.photo.large}")
+            ),
+            Container(
+              height: 50.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: (){
+                      Firestore.instance.runTransaction((tran) async{
+                        await tran.delete(newsAtIndex.reference);
+                        print("newsAtIndex.reference.documentID ${newsAtIndex.reference.documentID} deleted");
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  int _inc = 0;
+
+  _buildAppBar() {
+    return AppBar(
+      title: Text("News"),
+      actions: <Widget>[
+        IconButton(onPressed: (){
+          _inc++;
+          Firestore.instance.runTransaction((tran) async{
+            var ref = Firestore.instance.collection("news");
+            Photo p = Photo();
+            List<Comment> c = [];
+            c.add(Comment());
+            News news = News(title: "my title $_inc", photo: p, comments: c);
+            await ref.add(news.toMap());
+          }).whenComplete(() => print("news added"));
+        }, icon: Icon(Icons.add_circle),),
+      ],
+    );
+  }
+
 }
